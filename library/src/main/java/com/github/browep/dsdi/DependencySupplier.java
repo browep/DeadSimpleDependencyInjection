d52756303e231523637cabac5730ecdb16a67f40
Paul Brower <paulbrower@REMpbrower9FG8WM.local>
2016-12-20 12:44:06 -0700

    allow for injection of a super class

--- library/src/main/java/com/github/browep/dsdi/DependencySupplier.java
+++ library/src/main/java/com/github/browep/dsdi/DependencySupplier.java
@@ -2,6 +2,10 @@ package com.github.browep.dsdi;
 
 import android.util.Log;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -31,7 +35,8 @@ public abstract class DependencySupplier {
     }
 
     /**
-     * dyanmically load the {@link DependencySupplier} class
+     * Helper/Util method.
+     * dyanmically load the {@link DependencySupplier} class and initialize it
      * @param log
      * @param className
      * @return
@@ -70,26 +75,46 @@ public abstract class DependencySupplier {
     /**
      * call this on all classes that have @Inject annotations
      *
-     * @param obj
+     * @param obj Object of a Class that contains fields with @Inject annotations.
      */
     public void inject(Object obj) {
+
         if (obj == null) {
             throw new IllegalArgumentException("obj cannot be null");
         } else {
-            Field[] fields = obj.getClass().getDeclaredFields();
-            for (Field field : fields) {
-                if (field.isAnnotationPresent(Inject.class)) {
-                    field.setAccessible(true);
-                    Class fieldClass = field.getType();
-                    Object supply = supply(obj, fieldClass);
-                    log("injection: (" + obj +") " + field.getName() +" -> " + supply);
-                    try {
-                        field.set(obj, supply);
-                    } catch (IllegalAccessException e) {
-                        throw new RuntimeException(e);
-                    }
+            Class<?> objClass = obj.getClass();
+            innerInject(obj, objClass);
+        }
+    }
+
+    /**
+     * do the actual injection.  If no injection candidates are found, look in the super class
+     * @param obj
+     * @param objClass
+     * @return
+     */
+    private void innerInject(Object obj, Class<?> objClass) {
+
+        // get all the fields that this class ( but not it's super class ) explicitly declares
+        Field[] fields = objClass.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                field.setAccessible(true);
+                Class fieldClass = field.getType();
+                Object supply = supply(obj, fieldClass);
+                log("injection: (" + obj +") " + field.getName() +" -> " + supply);
+                try {
+                    field.set(obj, supply);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
                 }
             }
+        }
+
+        // if the superclass isnt Object and it has the @HasInjectees annotation, inject it as well
+        Class<?> superclass = objClass.getSuperclass();
+        if (superclass != Object.class && superclass.isAnnotationPresent(HasInjectees.class)) {
+            innerInject(obj, obj.getClass().getSuperclass());
         }
     }
 
@@ -97,5 +122,17 @@ public abstract class DependencySupplier {
         if(log) {
             Log.d(getClass().getCanonicalName(), logMsg);
         }
+    }
+
+    /**
+     *
+     * Explicit declaration that a class has @Inject fields.  Use this for a superclass that wants
+     * to be injected.  Not needed if the class will not be extended
+     *
+     * Created by paulbrower on 12/20/16.
+     */
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public static @interface HasInjectees {
     }
 }
